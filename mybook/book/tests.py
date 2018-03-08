c111248dff0eee2ac428345cccca2e7bfc7cad16
rkdgusrnrlrl <rkdgusrnrlrl@gmail.com>
2018-03-08 22:01:56 +0900

    fix:사용자 생성을 create_user 를 사용 하도록 수정 직접 save 하는 것은 비권장

--- mybook/book/tests.py
+++ mybook/book/tests.py
@@ -11,8 +11,15 @@ def test_user_count():
     assert User.objects.count() == 0
 
 @pytest.mark.django_db
+def test_user_create_without_email():
+    with pytest.raises(ValueError):
+        me = User.objects.create_user(name="강현구", email="")
+        me.save()
+
+
+@pytest.mark.django_db
 def test_user_create():
-    me = User(name="강현구", nick_name="rkdgusrnrlrl")
+    me = User.objects.create_user(email="rkdgusrnrlrl@gmail.com", name="강현구", nick_name="rkdgusrnrlrl")
     me.save()
     assert User.objects.count() == 1
 
