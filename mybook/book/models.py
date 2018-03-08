2a7582a4e8cfbd8ebf5ef74bdf85139bc3e22e4b
rkdgusrnrlrl <rkdgusrnrlrl@gmail.com>
2018-03-08 22:00:37 +0900

    fix:nick_name 을 추가 할 수 있게 수정

--- mybook/book/models.py
+++ mybook/book/models.py
@@ -18,11 +18,11 @@ class Book(models.Model):
             return self.title
 
 class UserManager(BaseUserManager):
-    def create_user(self, email, name, password=None):
+    def create_user(self, email, name, nick_name=None, password=None):
         if not email:
             raise ValueError("User must have an email")
 
-        user = self.model(email=email, name=name)
+        user = self.model(email=email, name=name, nick_name=nick_name)
         user.set_password(password)
         user.save()
         return user
