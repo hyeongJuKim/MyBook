a749150e4a8e34fb19b0b1b54f68cd63afcac193
rkdgusrnrlrl <rkdgusrnrlrl@gmail.com>
2018-03-16 23:13:47 +0900

    fix:user_view 전체 import 함

--- mybook/book/views.py
+++ mybook/book/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
 from django.urls import reverse_lazy
 from .models import Book
-from .user_view import UserCV, UserDV
+from .user_view import *
 
 
 def main(request):
