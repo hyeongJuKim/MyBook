485868335e38f99be906a7694a51261388185fbc
rkdgusrnrlrl <rkdgusrnrlrl@gmail.com>
2018-03-16 23:17:36 +0900

    fix:로그인 뷰 마이페이지 뷰 추가

--- mybook/book/user_view.py
+++ mybook/book/user_view.py
@@ -1,6 +1,9 @@
-from django.views.generic import CreateView, FormView, DetailView
+from django.views.generic import FormView, DetailView, TemplateView
+from django.contrib.auth.views import LoginView
 from .models import User
-from .forms import UserCreationForm
+from .forms import UserCreationForm, UserLoginForm
+from django.urls import reverse
+
 
 class UserCV(FormView):
     form_class = UserCreationForm
@@ -12,8 +15,8 @@ class UserCV(FormView):
         return super().form_valid(form)
 
     def get_success_url(self):
-        url = self.object.get_absolute_url()
-        return url
+        return reverse('login')
+
 
 class UserDV(DetailView):
     model = User
@@ -21,11 +24,20 @@ class UserDV(DetailView):
     context_object_name = 'user'
 
 
+class MyPageView(TemplateView):
+    model = User
+    template_name = 'book/user_detail.html'
+
+    def get_context_data(self, **kwargs):
+        print(self.request.user)
+        return {'user' : self.request.user}
 
 
 
+class UserLoginView(LoginView):
+    authentication_form = UserLoginForm
+    template_name = 'book/user_login.html'
+    context_object_name = 'user'
 
-
-
-
-
+    def get_success_url(self):
+        return reverse('mypage')
